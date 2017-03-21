fd4315316e3dcdc917e96a672a41923ae8b2c0cd
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-03-20 21:22:18 -0300

    Refactor js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
 const canFelipeDrink = false;
+
+let stopTimer = null;
 
 const felipeWillBeDrinking = 'March 22 2017';
 
@@ -22,6 +23,13 @@ function getRemainingTime() {
 function renderTimer(node) {
   const remainingTime = getRemainingTime();
 
+  if(remainingTime.days < 0) {
+    document.querySelector('#no').remove();
+    node.remove();
+    appendToDiv(createH1('yes'));
+    stopTimer();
+  }
+
   const timerContent =  `${remainingTime.days} days - ` +
                         `${remainingTime.hours} hours - ` +
                         `${remainingTime.minutes} minutes - ` +
@@ -33,6 +41,9 @@ function createH1(message) {
   const node = createHeader()
 
   node.innerText = message;
+
+  node.id = message;
+
   return node;
 }
 
@@ -64,7 +75,11 @@ function main() {
   }else {
     appendToDiv(createH1('no'));
 
-    setInterval(generateRenderFunction(), 1000);
+    const interval = setInterval(generateRenderFunction(), 1000);
+
+    stopTimer = function() {
+      clearInterval(interval);
+    }
   }
 }
 
